9e0d1cfcceb1e4611419a2e3306c6ac225c0373a
sqrt <sqrt@octoblu.com>
2015-10-15 17:50:58 -0700

    TIMEOUT is in seconds, not ms

--- circularqueue.go
+++ circularqueue.go
@@ -22,7 +22,7 @@ func New(conn Conn) (*ConnQueue) {
 
 // Pop returns a job from the queue and auto-readds it
 func (queue *ConnQueue) Pop() (Job,error) {
-  result, err := queue.conn.Do("BRPOPLPUSH", "circular-job-queue", "circular-job-queue", 1000)
+  result, err := queue.conn.Do("BRPOPLPUSH", "circular-job-queue", "circular-job-queue", 1)
   if err != nil {
     return nil, err
   }
